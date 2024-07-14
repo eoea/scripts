6d46ee96c7b990a3840624e6f3893547611880bd
eoea <eoea754@gmail.com>
2024-07-14 10:20:24 +0400

    initial commit

---
+++ steam_deck.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Script to help set up my Steam Deck environment.
+#
+
+# Stop/reduce video lag/stutter when in Desktop mode.
+# source: https://steamcommunity.com/app/1675200/discussions/1/3395176266123951588/
+flatpak install org.freedesktop.Platform.ffmpeg-full
