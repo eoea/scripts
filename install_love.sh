bc04d7c3ec1cb60ac31501babce972591c394aed
eoea <eoea754@gmail.com>
2025-06-21 11:59:17 +0400

    fmt

--- install_love.sh
+++ install_love.sh
@@ -4,20 +4,19 @@
 #
 # Created by Emile O.E. Antat (eoea) <eoea754@gmail.com>
 
-
 main() {
 	local url
-  local version
+	local version
 
-  version=11.5
+	version=11.5
 
 	case "$(uname -s)" in
 	Darwin)
 		if [[ "$(uname -m)" == "arm64" ]]; then
-      url="https://github.com/love2d/love/releases/download/${version}/love-${version}-macos.zip"
-      curl -L "${url}" -o "${HOME}/Downloads/love.zip"
-      unzip -o "${HOME}/Downloads/love.zip" -d "${HOME}/Applications"
-      ln -sfv "${HOME}/Applications/love.app/Contents/MacOS/love" "${HOME}/.local/bin/love"
+			url="https://github.com/love2d/love/releases/download/${version}/love-${version}-macos.zip"
+			curl -L "${url}" -o "${HOME}/Downloads/love.zip"
+			unzip -o "${HOME}/Downloads/love.zip" -d "${HOME}/Applications"
+			ln -sfv "${HOME}/Applications/love.app/Contents/MacOS/love" "${HOME}/.local/bin/love"
 		fi
 		;;
 	*)
