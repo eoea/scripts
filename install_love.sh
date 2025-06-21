a16950966266738db98d3d2c462a16c80c791df0
eoea <eoea754@gmail.com>
2025-06-21 11:53:26 +0400

    initial commit

---
+++ install_love.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# This is a script that downloads and installs Love2d on MacOS.
+#
+# Created by Emile O.E. Antat (eoea) <eoea754@gmail.com>
+
+
+main() {
+	local url
+  local version
+
+  version=11.5
+
+	case "$(uname -s)" in
+	Darwin)
+		if [[ "$(uname -m)" == "arm64" ]]; then
+      url="https://github.com/love2d/love/releases/download/${version}/love-${version}-macos.zip"
+      curl -L "${url}" -o "${HOME}/Downloads/love.zip"
+      unzip -o "${HOME}/Downloads/love.zip" -d "${HOME}/Applications"
+      ln -sfv "${HOME}/Applications/love.app/Contents/MacOS/love" "${HOME}/.local/bin/love"
+		fi
+		;;
+	*)
+		echo "No download for $(uname -s) $(uname -m)" >&2
+		return 1
+		;;
+	esac
+
+}
+
+main "$@"
